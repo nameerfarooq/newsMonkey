580881940627921efa3c93d43abd6e9be587bc9f
nameerfarooq <61945782+nameerfarooq@users.noreply.github.com>
2021-09-15 09:01:37 +0500

    15sep21
    infinite loader implemented

--- src/App.js
+++ src/App.js
@@ -10,19 +10,20 @@ import {
   
 } from "react-router-dom";
 export default class App extends Component {
+  pageSize = 6
   render() {
     return (
       <Router>
       
         <Navbar/>
         <Switch>
-          <Route exact path="/business" ><News key="business" pageSize={12} country="us" category="business" /></Route>
-          <Route exact path="/entertainment" ><News key="entertainment"  pageSize={12} country="us" category="entertainment" /></Route>
-          <Route exact path="/" ><News key="general"  pageSize={12} country="us" category="general" /></Route>
-          <Route exact path="/health" ><News key="health"  pageSize={12} country="us" category="health" /></Route>
-          <Route exact path="/science" ><News key="science"  pageSize={12} country="us" category="science" /></Route>
-          <Route exact path="/sports" ><News key="sports"  pageSize={12} country="us" category="sports" /></Route>
-          <Route exact path="/technology" ><News key="technology"  pageSize={12} country="us" category="technology" /></Route>
+          <Route exact path="/business" ><News key="business" pageSize={this.pageSize} country="us" category="business" /></Route>
+          <Route exact path="/entertainment" ><News key="entertainment"  pageSize={this.pageSize} country="us" category="entertainment" /></Route>
+          <Route exact path="/" ><News key="general"  pageSize={this.pageSize} country="us" category="general" /></Route>
+          <Route exact path="/health" ><News key="health"  pageSize={this.pageSize} country="us" category="health" /></Route>
+          <Route exact path="/science" ><News key="science"  pageSize={this.pageSize} country="us" category="science" /></Route>
+          <Route exact path="/sports" ><News key="sports"  pageSize={this.pageSize} country="us" category="sports" /></Route>
+          <Route exact path="/technology" ><News key="technology"  pageSize={this.pageSize} country="us" category="technology" /></Route>
         </Switch>
       
       </Router>
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -3,10 +3,15 @@ import './newsitem.css'
 import Notfound from './notfound'
 export class NewsItems extends Component {
     render() {
-let {title, description, ImageUrl, newsUrl} = this.props
+let {title, description, ImageUrl, newsUrl, publishedAt, author,source} = this.props
         return (
             <div>
-                <div className="card m-3" style={{width: "20rem"}}>
+                       <div className="card m-3" style={{width: "20rem"}}>
+                       <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:"80%",zIndex:"1"}}>
+                           {source.name}
+    
+                            </span>
+          
                     
                     
                     {ImageUrl?  <img src={ImageUrl } className="card-img-top imgCard" alt="..."/>:<Notfound/>}  
@@ -14,6 +19,7 @@ let {title, description, ImageUrl, newsUrl} = this.props
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-muted"> from {!author?"unknown":author} on {new Date(publishedAt).toUTCString()} </small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
                     </div>
                 </div>
